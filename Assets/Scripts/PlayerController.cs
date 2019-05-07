9d998999e7f0d2db66446f59e7f657d4b4012c8c
Douglas Urner <dlu@canishe.com>
2019-05-07 09:53:17 -0700

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public string axisX = "Horizontal";
+    public string axisY = "Vertical";
+
+    private Rigidbody2D rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var moveX = Input.GetAxis(axisX);
+        var moveY = Input.GetAxis(axisY);
+
+        rb.velocity = new Vector2(moveX * speed, moveY * speed);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("OnCollisionEnter2D() - collided with: " + other);
+//        throw new System.NotImplementedException();
+//       switch (other.collider)
+//        {
+//            
+//        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Unity Examples Basic 2D Sprite Controller
