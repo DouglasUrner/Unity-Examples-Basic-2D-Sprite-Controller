9dd97aac6c27f3c1d1247b0e748e57f73b015672
Douglas Urner <dlu@canishe.com>
2019-05-07 14:13:16 -0700

    Collision actions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 1.0f;
+    public float speedStep = 0;
     public string axisX = "Horizontal";
     public string axisY = "Vertical";
 
     private Rigidbody2D rb;
-    
+    private int sceneNumber = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,31 +27,67 @@ public class PlayerController : MonoBehaviour
         rb.velocity = new Vector2(moveX * speed, moveY * speed);
     }
 
+    /*
+     * OnCollisionEnter() is called whenever we collide with another game
+     * object with a collider component. We are passed a Collision 2D object,
+     * other, that we can examine to determine what we collided with. When we
+     * collide with an object the behavior of our sprite and of the object we
+     * collided with is determined by the Unity physics engine - if you just
+     * want to detect the collision, use OnTriggerEnter() and set the IsTrigger
+     * property on the game object's collider.
+     */
     void OnCollisionEnter2D(Collision2D other)
     {
-
         switch (other.gameObject.tag)
         {
             case "Memory":
                 Debug.Log("Player Controller: OnCollisionEnter2D(): collision with Memory: '"
                           + other.gameObject.name + "'");
+                // Inform the player about the memory.
+                Describe(other.gameObject);
                 break;
-            
+
             case "Obstacle":
                 Debug.Log("Player Controller: OnCollisionEnter2D(): collision with Obstacle: '"
                           + other.gameObject.name + "'");
+                // Destroy the obstacle.
+                Destroy(other.gameObject);
                 break;
-            
+
             case "Passage":
+                /*
+                 * This should probably be in the OnTriggerEnter() handler, so that the feel of entering
+                 * the passage is more realistic. But you could also set up an invisible sprite and destroy
+                 * it when you collide with it - or maybe just load the new scene...
+                 */
                 Debug.Log("Player Controller: OnCollisionEnter2D(): collision with Passage: '"
                           + other.gameObject.name + "'");
+
+                LoadNextScene(++sceneNumber);
+                // Go a little faster on the next level.
+                speed = speed + speedStep;
                 break;
-            
+
+            case "Wall":
+                // Nothing to do here (at least for now).
+                break;
+
             default:
                 Debug.Log("Player Controller: OnCollisionEnter2D(): collision with untagged object: '"
                           + other.gameObject.name + "'");
                 // TODO: throw exception?
                 break;
         }
+    }
+
+    void Describe(GameObject memory)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    void LoadNextScene(int sceneNumber)
+    {
+        // throw new System.NotImplementedException();
+        Debug.Log("PlayerController: LoadNextScene(): loading scene '" + sceneNumber + "'");
     }
 }
