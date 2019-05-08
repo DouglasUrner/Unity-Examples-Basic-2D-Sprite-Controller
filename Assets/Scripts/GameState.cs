98f840f39319a4bd82c0ad26ad5d2d921ca788cd
Douglas Urner <dlu@canishe.com>
2019-05-07 17:02:16 -0700

    Update GameState code.

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameState
+{
+    // Game state variables that need to persist between levels.
+    private static int _sceneNumber = 0;
+
+    // Setters & getters to provide access to state variables.
+    public static int SceneNumber
+    {
+        get { return _sceneNumber; }
+        set { _sceneNumber = value; }
+    }
+}
