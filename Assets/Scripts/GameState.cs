c997d362c689067430adf6ce38623685961c7520
Douglas Urner <dlu@canishe.com>
2019-05-08 06:54:51 -0700

    Add PlayerTransform to saveable state.

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -6,11 +6,18 @@ public static class GameState
 {
     // Game state variables that need to persist between levels.
     private static int _sceneNumber = 0;
+    private static Transform _playerTransform;
 
     // Setters & getters to provide access to state variables.
     public static int SceneNumber
     {
         get { return _sceneNumber; }
         set { _sceneNumber = value; }
+    }
+
+    // Pass the player's position between scenes.
+    public static Transform PlayerTransform {
+        get;
+        set;
     }
 }
