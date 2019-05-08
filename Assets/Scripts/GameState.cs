02eb6adefdedecc8dd2f44513b619aed4d040ecb
Douglas Urner <dlu@canishe.com>
2019-05-08 07:27:41 -0700

    Add comments.

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -2,13 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+** When loading a new scene, Unity destroys all of the game objects in
+** the old scene. Information that needs to persist between scenes has
+** to be "stashed" somewhere before loading the new scene and then
+** recovered before it is used again in the new scene. That is the purpose
+** of the GameState class.
+*/
+
 public static class GameState
 {
-    // Game state variables that need to persist between levels.
+    /*
+     * Game state variables that need to persist between levels.
+     */
     private static int _sceneNumber = 0;
     private static Transform _playerTransform;
 
-    // Setters & getters to provide access to state variables.
+    /*
+     * Setters & getters to provide access to state variables.
+     */
+
+    // Index of the currently loaded scene.
     public static int SceneNumber
     {
         get { return _sceneNumber; }
